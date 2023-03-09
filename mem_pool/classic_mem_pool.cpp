7d3760bfadba03f16f19c6d5f13c6259e115b004
xby999 <1963256383@qq.com>
2023-03-09 18:07:54 +0800

    Update classic_mem_pool.cpp
    Finish

--- mem_pool/classic_mem_pool.cpp
+++ mem_pool/classic_mem_pool.cpp
@@ -30,6 +30,7 @@ private:
 public:
    MemPool()
    {
+      std::cout<<"sadas"<<std::endl;
       freeNodeHeader = NULL;
       memBlockHeader = NULL;
    }
@@ -53,26 +54,105 @@ template <int ObjectSize, int NumofObjects>
 void *MemPool<ObjectSize, NumofObjects>::malloc()
 {
    // 无空闲节点，申请新内存块
-   if (freeNodeHeader->pNext == NULL)
+   if (freeNodeHeader == NULL)
    {
+      std::cout<<"sadas"<<std::endl;
       MemBlock *newBlock = new MemBlock;
       newBlock->pNext = NULL;
 
-      freeNodeHeader= &newBlock->data[0]; //设置内存块第一个节点为空闲节点链表的表头
-                                          //->的优先级高于&,所以可以理解为&(newBlock->data[0])
-      
-      //将内存块其他节点串起
-      for(int i=0;i<NumofObjects;++i){
-         newBlock->data[i-1].pNext = &newBlock->data[i];
+      freeNodeHeader = &newBlock->data[0]; // 设置内存块第一个节点为空闲节点链表的表头
+                                           //->的优先级高于&,所以可以理解为&(newBlock->data[0])
+
+      // 将内存块其他节点串起
+      for (int i = 0; i < NumofObjects; ++i)
+      {
+         newBlock->data[i - 1].pNext = &newBlock->data[i];
       }
-      newBlock->data[NumofObjects-1].pNext=NULL;
+      newBlock->data[NumofObjects - 1].pNext = NULL;
+
+      // allocate memory for the first time
+      if (memBlockHeader == NULL)
+      {
+         memBlockHeader = newBlock;
+      }
+      else
+      {
+         newBlock->pNext = memBlockHeader;
+         memBlockHeader = newBlock;
+      }
    }
+
+   void *freeNode = freeNodeHeader;
+   freeNodeHeader = freeNodeHeader->pNext;
+   return freeNode;
 }
 
 // void free(void*)实现，释放已分配节点
+template <int ObjectSize, int NumofObject>
+void MemPool<ObjectSize, NumofObject>::free(void *p)
+{
+   FreeNode *pNode = (FreeNode *)p;
+   pNode->pNext = freeNodeHeader;
+   freeNodeHeader = pNode;
+   std::cout<<"sadas";
+}
+
+class ActualClass
+{
+   static int count;
+   int No;
+
+public:
+   ActualClass()
+   {
+      No = count;
+      count++;
+   }
+   void print()
+   {
+      std::cout << ": ";
+      std::cout << this << ": ";
+      std::cout << "the " << No << "th obj" << std::endl;
+   }
+
+   void *operator new(size_t size);
+   void operator delete(void *p);
+};
+
+MemPool<sizeof(ActualClass), 2> MP; // 这一步是预先申请一个内存池
+
+void *ActualClass::operator new(size_t size)
+{
+   return MP.malloc();
+}
+
+void ActualClass::operator delete(void *p)
+{
+   return MP.free(p);
+}
+
+int ActualClass::count = 0;
 
 int main()
 {
+   ActualClass *p1= new ActualClass;
+   p1->print();
+   std::cout<<"asdasd"<<std::endl;
 
+   delete p1;
+
+   // ActualClass *p2= new ActualClass;
+   // p2->print();
+
+   // delete p1;
+   // p1= new ActualClass;
+   // p1->print();
+
+   // ActualClass *p3= new ActualClass;
+   // p3->print();
+
+   // delete p1;
+   // delete p2;
+   // delete p3;
    return 0;
 }
